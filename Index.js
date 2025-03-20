e5d311b78e6ea4028cb0a9b38e2e130edfdd6158
DaviGayDaSilva <languith54@gmail.com>
2025-03-20 18:34:48 -0300

    Index.js

---
+++ Index.js
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Super Site Tudo-em-Um</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <h1>Bem-vindo ao Super Site!</h1>
+    <nav>
+        <ul>
+            <li><a href="chat.html">Chat</a></li>
+            <li><a href="editor.html">Editor de Texto</a></li>
+            <li><a href="social.html">Rede Social</a></li>
+            <li><a href="emulador.html">Emulador de Windows</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
